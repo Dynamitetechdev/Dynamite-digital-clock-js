d2886ccbbe9078b8ca7dc4fe7ed67ec82be0936f
Dynamite tech <oluwapoanu87@gmail.com>
2022-07-07 01:23:44 +0100

    Digital Clock Files

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ function displayOurTime(){
     let timeHour = date.getHours();
     let timeMinute = date.getMinutes();
     let timeSecond = date.getSeconds();
-
     if(timeHour >= 12){
-        document.querySelector(".time_session").innerHTML = "PM"
+        document.querySelector(".time_session").innerHTML = "PM";
+        document.querySelector(".watch").style.color = "#000000"
+        document.querySelector("body").style.background = "#ffffff"
     } else{
         document.querySelector(".time_session").innerHTML = "AM"
+        document.querySelector(".watch").style.color = "#ffffff"
+        document.querySelector("body").style.background = "#000000"
     }
 
 
